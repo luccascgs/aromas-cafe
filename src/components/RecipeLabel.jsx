678297a9b3d0814d1f1dfa7b296f3780fc0bd552
Luccas Gomes <luccascorreia.08@hotmail.com>
2023-10-05 14:54:42 -0300

    feat: add main image

--- src/components/RecipeLabel.jsx
+++ src/components/RecipeLabel.jsx
@@ -2,7 +2,7 @@ const RecipeLabel = (props) => {
     return (
         <div className="d-flex flex-column align-items-center mw-fit-content">
             <label className="bg-warning px-5 py-1 recipeType">{props.type}</label>
-            <div className="d-flex flex-column align-items-center bg-white p-4 rounded-3">
+            <div className="shadow-sm d-flex flex-column align-items-center bg-white p-4 rounded-3">
                 <img className="recipe-image-size object-fit-cover border rounded" src={props.img} alt={props.recipe} />
                 <h4 className="font-weight-bolder mt-2">{props.recipe}</h4>
                 <footer className="d-flex flex-column align-items-center">
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -4,8 +4,16 @@ export default function HomeScreen() {
     return (
         <div>
             <div>
-                <span className="texto-teste">AAAAAAAA</span>
-                <img className="imagem-teste" src="https://ideas.ted.com/wp-content/uploads/sites/3/2022/07/FINAL_Coffee.jpg" />
+                <div className="content-up position-absolute w-100 h-80vh d-flex flex-column justify-content-center align-items-center ml-10vw text-white z-2">
+                    <div className="w-75vw">
+                        <h1><b>Bem vindo!</b></h1>
+                        <h2 className="w-50">Aromas de Café, onde a magia do café acontece.</h2>
+                    </div>
+                </div>
+                <div className="z-n1">
+                    <div className="overlay-black"> </div>
+                    <img className="w-100 object-fit-cover h-80vh" src="https://ideas.ted.com/wp-content/uploads/sites/3/2022/07/FINAL_Coffee.jpg" alt="Café" />
+                </div>
             </div>
             <h1>Eu sou a HomeScreen Divina!</h1>
             <RecipeLabel recipe={"Capuccino"} type={"Café Quente"} time={"5"} portion={"2"} img={"https://www.mococa.com.br/wp-content/uploads/2022/07/Cappuccino-com-Mocoquinha_4-scaled.jpg"} />
